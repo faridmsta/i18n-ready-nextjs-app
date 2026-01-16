eae5df77a5cbb46b653d5ed7456111db80570263
faridmsta <faridmustafyev@gmail.com>
2026-01-17 00:31:23 +0400

    commit

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
+import { useTranslations } from 'next-intl';
+import { use } from "react";
+import { setRequestLocale } from "next-intl/server";
+import LocaleSwitcher from "@/components/LocaleSwitcher";
 
-export default function Home() {
+export default function Home({
+  params
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+
+  // IN all pages/components that use translations, set the request locale (to mak sure server knows the current locale)
+  const { locale } = use(params);
+  setRequestLocale(locale);
+
+  const t = useTranslations('home');
+  
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,8 +30,9 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            {t('welcome')}
           </h1>
+          <LocaleSwitcher />
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
